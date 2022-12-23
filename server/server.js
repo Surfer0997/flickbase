a9613f1d0fcf9bc222d77ce421563b48cbec06fa
Surfer0997 <artanagibaet228@gmail.com>
2022-12-23 21:25:31 +0200

    fd

--- server/server.js
+++ server/server.js
@@ -42,14 +42,15 @@ app.use((err,req,res,next)=>{
 });
 
 
-app.use(express.static('client/build')); // for deploy
-if (process.env.NODE_ENV === 'production') {
+app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
     const path = require('path');
 
-    app.get('/*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
     })
 }
+
 
 app.listen(PORT, ()=>{
     console.log('Server running on port', PORT);
